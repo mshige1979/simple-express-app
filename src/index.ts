2192e75f1c37cb5cfcddb7eec9289a4e302ee9b4
mshige1979 <mshige1979@ryuouen.net>
2022-11-06 06:41:33 +0900

    viewファイル対応

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-const express = require("express");
+import express from "express";
+import mustacheExpress from "mustache-express";
+import bodyParser from 'body-parser';
 const app = express();
 const port = 5000;
 
-app.get("/", (req: any, res: any) => {
-  res.send("Hello World! App Runner Test3-3");
+// template
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
+app.use(express.static("public"));
+
+// 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  //res.send("Hello World! App Runner Test3-3");
+  res.render('index');
 });
 
 app.listen(port, () => {
