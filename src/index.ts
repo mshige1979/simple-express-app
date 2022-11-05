e2211e5097018bfe5937fedaedfd5fcd96a9b026
mshige1979 <mshige1979@ryuouen.net>
2022-11-05 23:19:51 +0900

    デプロイテスト

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req: any, res: any) => {
-  res.send("Hello World! App Runner Test3-2");
+  res.send("Hello World! App Runner Test3-3");
 });
 
 app.listen(port, () => {
